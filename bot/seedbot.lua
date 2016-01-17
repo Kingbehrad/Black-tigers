6da11c61f33e4925a599ad30af0d858ad23671ea
Kingbehrad <Hosseini.arsham62.ah@gmail.com>
2016-01-17 07:46:46 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -221,7 +221,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
@@ -508,7 +508,7 @@ function cron_plugins()
 end
 
 -- Start and load values
-our_id = 0
+our_id = 100777365
 now = os.time()
 math.randomseed(now)
 started = false
